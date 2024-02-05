2f0487bd649d21d8a6f058ffe978672a2672a75f
PinhoFX <pinhofxcomercial@gmail.com>
2024-02-05 14:57:56 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./index.html","./atividades.html","./index2.html"],
   theme: {
+  //   colors:{
+  //     'primary': '#1200ff',
+               
+  //     'secondary': '#00b17b',
+               
+  //     'accent': '#ab2a00',
+               
+  //     'neutral': '#231718',
+               
+  //     'base-100': '#262626',
+               
+  //     'info': '#00c6ff',
+               
+  //     'success': '#238100',
+               
+  //     'warning': '#d36c00',
+               
+  //     'error': '#ff6692',
+  // },
     extend: {
+    
       height: {
         '128': '50px',
       },
